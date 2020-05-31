71c08cd0e74332e19e5c98b9b125d01ba105c434
Vishukumar M <vishukumar.m@ericsson.com>
2020-06-01 00:10:54 +0530

    added react deployment plugin in pom

--- src/main/java/com/theuselesscompany/controller/CourseResource.java
+++ src/main/java/com/theuselesscompany/controller/CourseResource.java
@@ -3,11 +3,12 @@ package com.theuselesscompany.controller;
 import com.theuselesscompany.model.Course;
 import com.theuselesscompany.service.CoursesHardcodedService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import java.net.URI;
 import java.util.List;
 
 @CrossOrigin(origins = { "http://localhost:3000", "http://localhost:4200" })
@@ -20,5 +21,50 @@ public class CourseResource {
 	@GetMapping("/api/instructors/{username}/courses")
 	public List<Course> getAllCourses(@PathVariable String username) {
 		return courseManagementService.findAll();
+	}
+
+	@GetMapping("/api/instructors/{username}/courses/count")
+	public Long getAllCoursesCount(@PathVariable String username) {
+		return Long.valueOf(courseManagementService.findAll().size());
+	}
+
+	@GetMapping("/api/instructors/{username}/courses/{id}")
+	public Course getCourse(@PathVariable String username, @PathVariable long id) {
+		return courseManagementService.findById(id);
+	}
+
+	@DeleteMapping("/api/instructors/{username}/courses/{id}")
+	public ResponseEntity<Void> deleteCourse(@PathVariable String username, @PathVariable long id) {
+
+		Course course = courseManagementService.deleteById(id);
+
+		if (course != null) {
+			return ResponseEntity.noContent().build();
+		}
+
+		return ResponseEntity.notFound().build();
+	}
+
+	@PutMapping("/api/instructors/{username}/courses/{id}")
+	public ResponseEntity<Course> updateCourse(@PathVariable String username, @PathVariable long id,
+											   @RequestBody Course course) {
+
+		Course courseUpdated = courseManagementService.save(course);
+
+		return new ResponseEntity<Course>(course, HttpStatus.OK);
+	}
+
+	@PostMapping("/api/instructors/{username}/courses")
+	public ResponseEntity<Void> createCourse(@PathVariable String username, @RequestBody Course course) {
+
+		Course createdCourse = courseManagementService.save(course);
+
+		// Location
+		// Get current resource url
+		/// {id}
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(createdCourse.getId())
+				.toUri();
+
+		return ResponseEntity.created(uri).build();
 	}
 }
--- src/main/java/com/theuselesscompany/service/CoursesHardcodedService.java
+++ src/main/java/com/theuselesscompany/service/CoursesHardcodedService.java
@@ -13,14 +13,48 @@ public class CoursesHardcodedService {
 	private static long idCounter = 0;
 
 	static {
-		courses.add(new Course(++idCounter, "in28minutes", "Learn Full stack with Spring Boot and Angular"));
-		courses.add(new Course(++idCounter, "in28minutes", "Learn Full stack with Spring Boot and React"));
-		courses.add(new Course(++idCounter, "in28minutes", "Master Microservices with Spring Boot and Spring Cloud"));
-		courses.add(new Course(++idCounter, "in28minutes",
+		courses.add(new Course(++idCounter, "course1", "Learn Full stack with Spring Boot and Angular"));
+		courses.add(new Course(++idCounter, "course2", "Learn Full stack with Spring Boot and React"));
+		courses.add(new Course(++idCounter, "course3", "Master Microservices with Spring Boot and Spring Cloud"));
+		courses.add(new Course(++idCounter, "course4",
 				"Deploy Spring Boot Microservices to Cloud with Docker and Kubernetes"));
 	}
 
 	public List<Course> findAll() {
 		return courses;
+	}
+
+	public Course save(Course course) {
+		if (course.getId() == -1 || course.getId() == 0) {
+			course.setId(++idCounter);
+			courses.add(course);
+		} else {
+			deleteById(course.getId());
+			courses.add(course);
+		}
+		return course;
+	}
+
+	public Course deleteById(long id) {
+		Course course = findById(id);
+
+		if (course == null)
+			return null;
+
+		if (courses.remove(course)) {
+			return course;
+		}
+
+		return null;
+	}
+
+	public Course findById(long id) {
+		for (Course course : courses) {
+			if (course.getId() == id) {
+				return course;
+			}
+		}
+
+		return null;
 	}
 }
